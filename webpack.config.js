eaf491885918182eb58fdef4e9a2eb0d4e6b7729
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2023-10-10 23:57:17 +0300

    сверстал шапку, и 2 страницу

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 3030, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true, // сайт будет открываться сам при запуске npm run dev
+  },
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist/"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: "babel-loader",
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(s*)css/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
